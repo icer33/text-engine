acdda506cf9967d069bea7c1de4ac5ec722bcc65
Anthony Nixon <icer33@gmail.com>
2023-03-23 21:10:00 -0400

    Added Glade adventure and 10 portal rooms

--- game-disks/glade-portal.js
+++ game-disks/glade-portal.js
@@ -1,92 +1,29 @@
 const demoDisk = () => ({
-  roomId: 'foyer', // the ID of the room the player starts in
+  roomId: 'glade', // the ID of the room the player starts in
   rooms: [
     {
-      id: 'foyer', // unique ID for this room
-      name: 'The Foyer', // room name (shown when player enters the room)
+      id: 'glade', // unique ID for this room
+      name: 'The Glade', // room name (shown when player enters the room)
       // room description (shown when player first enters the room)
-      desc: `Welcome to the **TEXT-ENGINE** demo disk! This disk is a text adventure game designed to introduce you to the features available to you in **text-engine**. Using this engine, you can make a text game of your own.
+      desc: `You find yourself in a serene glade surrounded by tall trees. 
+      Rays of sunlight filter through the leaves, creating a dappled effect on the soft green grass beneath your feet. 
+      In the center of the clearing, you notice ten glowing portals of different colors hovering above the ground, each pulsing with a gentle energy..
 
       Type **LOOK** to have a look around.`,
       // optional callback when player issues the LOOK command
       // here, we use it to change the foyer's description
       onLook: () => {
-        const room = getRoom('foyer');
-        room.desc = `You are currently standing in the foyer. There's a huge **MONSTERA** plant to your right, and a massive **WINDOW** to your left bathing the room in natural light. Both the **PLANT** and the **WINDOW** stretch to the ceiling, which must be at least 25 feet high.
+        const room = getRoom('glade');
+        room.desc = `You are currently standing in the forest glade surrounded by ten portals
 
-        ***Rooms** form the foundation of the engine's design. At any given time, your player will be standing in one of the rooms you built for them. These can be literal rooms like the foyer you find yourself in now, or metaphorical rooms like **The End of Time** or **A Dream**.
+        Places can have **exits** that take you to other rooms. For instance, **go portal1** will take you through **Portal 1**.
 
-        Each room you create should have a description. (That's what you're reading now!)
+        Places can also contain **items**. Sometimes the player can **TAKE** or **USE** items.
 
-        Rooms can have **exits** that take you to other rooms. For instance, to the **NORTH** is the **RECEPTION DESK**.
-
-        Rooms can also contain **items**. Sometimes the player can **TAKE** or **USE** items.
-
-        Type **ITEMS** to see a list of items in the foyer. Or type **HELP** to see what else you can do!`;
+        Type **ITEMS** to see a list of items in the glade. Or type **HELP** to see what else you can do!`;
       },
       // optional list of items in the room
       items: [
-        {
-          name: 'tall window', // the item's name
-          desc: `All you can see are puffy white clouds over a blue sky.`, // description shown when player looks at the item
-        },
-        {
-          name: ['monstera', 'plant', 'swiss cheese'], // player can refer to this item by any of these names
-          desc: `Sometimes called a Swiss Cheese plant, no office is complete without one. It has lovely, large leaves. This is the biggest you\'ve ever seen.
-
-          There's **SOMETHING SHINY** in the pot.`,
-          block: `It's far too large for you to carry.`, // optional reason player cannot pick up this item
-          // when player looks at the plant, they discover a shiny object which turns out to be a key
-          onLook: () => {
-            if (getItem('shiny')) {
-              // the key is already in the pot or the player's inventory
-              return;
-            }
-
-            const foyer = getRoom('foyer');
-
-            // put the silver key in the pot
-            foyer.items.push({
-              name: ['shiny thing', 'something shiny', 'pot'],
-              onUse() {
-                const room = getRoom(disk.roomId);
-                if (room.id === 'foyer') {
-                  println(`There's nothing to unlock in the foyer.`);
-                } else if (room.id === 'reception') {
-                  println(`You unlock the door to the **EAST**!`);
-                  // remove the block
-                  const exit = getExit('east', room.exits);
-                  delete exit.block;
-                  // this item can only be used once
-                  const key = getItem('shiny');
-                  key.onUse = () => println(`The lab has already been unlocked.`);
-                } else {
-                  println(`There's nothing to unlock here.`);
-                }
-              },
-              desc: `It's a silver **KEY**!`,
-              onLook() {
-                const key = getItem('shiny');
-
-                // now that we know it's a key, place that name first so the engine calls it by that name
-                key.name.unshift('silver key');
-
-                // let's also update the description
-                key.desc = `It has a blue cap with the word "LAB" printed on it.`;
-
-                // remove this method (we don't need it anymore)
-                delete key.onLook;
-              },
-              isTakeable: true,
-              onTake() {
-                println(`You took it.`);
-                // update the monstera's description, removing everything starting at the line break
-                const plant = getItem('plant');
-                plant.desc = plant.desc.slice(0, plant.desc.indexOf('\n'));
-              },
-            });
-          },
-        },
         {
           name: 'dime',
           desc: `Wow, ten cents.`,
@@ -106,9 +43,16 @@ const demoDisk = () => ({
       // places the player can go from this room
       exits: [
         // GO NORTH command leads to the Reception Desk
-        { dir: 'north', id: 'reception' },
-        { dir: "p1", id: 'portal_1' },
-        { dir: "p2", id: 'portal_2' },
+        { dir: ['portal1', 'p1'], id: 'portal_1' },
+        { dir: ['portal2', 'p2'], id: 'portal_2' },
+        { dir: ['portal3', 'p3'], id: 'portal_3' },
+        { dir: ['portal4', 'p4'], id: 'portal_4' },
+        { dir: ['portal5', 'p5'], id: 'portal_5' },
+        { dir: ['portal6', 'p6'], id: 'portal_6' },
+        { dir: ['portal7', 'p7'], id: 'portal_7' },
+        { dir: ['portal8', 'p8'], id: 'portal_8' },
+        { dir: ['portal9', 'p9'], id: 'portal_9' },
+        { dir: ['portal10', 'p10'], id: 'portal_10' },
       ],
     },
     {
--- game-disks/portal_2.js
+++ game-disks/portal_2.js
@@ -4,93 +4,24 @@ const portal_2 = () => ({
       id: 'portal_2', // unique ID for this room
       name: 'Portal 2', // room name (shown when player enters the room)
       // room description (shown when player first enters the room)
-      desc: `Portal 2's room.`,
+      desc: `Portal 2's room. It is rather empty except for an orb floating in the middle of the room.`,
       onLook: () => {
         const room = getRoom('portal_2');
-        room.desc = `This is the room leading from portal2`;
+        room.desc = `Your eyes are drawn to a large, shimmering orb hovering in mid-air, its surface pulsing with a warm, golden light.
+         The room around you is dimly lit, but the orb seems to fill the space with a comforting radiance.`;
       },
       items: [
         {
-          name: 'tall window', // the item's name
-          desc: `All you can see are puffy white clouds over a blue sky.`, // description shown when player looks at the item
-        },
-        {
-          name: ['monstera', 'plant', 'swiss cheese'], // player can refer to this item by any of these names
-          desc: `Sometimes called a Swiss Cheese plant, no office is complete without one. It has lovely, large leaves. This is the biggest you\'ve ever seen.
-
-          There's **SOMETHING SHINY** in the pot.`,
+          name: ['floating orb', 'orb'], // player can refer to this item by any of these names
+          desc: `Your eyes are drawn to a large, shimmering orb hovering in mid-air, its surface pulsing with a warm, golden light. 
+          The room around you is dimly lit, but the orb seems to fill the space with a comforting radiance.`, // description shown when player looks at the item
           block: `It's far too large for you to carry.`, // optional reason player cannot pick up this item
-          // when player looks at the plant, they discover a shiny object which turns out to be a key
-          onLook: () => {
-            if (getItem('shiny')) {
-              // the key is already in the pot or the player's inventory
-              return;
-            }
-
-            const foyer = getRoom('portal_1');
-
-            // put the silver key in the pot
-            foyer.items.push({
-              name: ['shiny thing', 'something shiny', 'pot'],
-              onUse() {
-                const room = getRoom(disk.roomId);
-                if (room.id === 'foyer') {
-                  println(`There's nothing to unlock in the foyer.`);
-                } else if (room.id === 'reception') {
-                  println(`You unlock the door to the **EAST**!`);
-                  // remove the block
-                  const exit = getExit('east', room.exits);
-                  delete exit.block;
-                  // this item can only be used once
-                  const key = getItem('shiny');
-                  key.onUse = () => println(`The lab has already been unlocked.`);
-                } else {
-                  println(`There's nothing to unlock here.`);
-                }
-              },
-              desc: `It's a silver **KEY**!`,
-              onLook() {
-                const key = getItem('shiny');
-
-                // now that we know it's a key, place that name first so the engine calls it by that name
-                key.name.unshift('silver key');
-
-                // let's also update the description
-                key.desc = `It has a blue cap with the word "LAB" printed on it.`;
-
-                // remove this method (we don't need it anymore)
-                delete key.onLook;
-              },
-              isTakeable: true,
-              onTake() {
-                println(`You took it.`);
-                // update the monstera's description, removing everything starting at the line break
-                const plant = getItem('plant');
-                plant.desc = plant.desc.slice(0, plant.desc.indexOf('\n'));
-              },
-            });
-          },
-        },
-        {
-          name: 'dime',
-          desc: `Wow, ten cents.`,
-          isTakeable: true, // allow the player to TAKE this item
-          onTake: () => println(`You bend down and pick up the tiny, shiny coin.
-
-          *Now it's in your **inventory**, and you can use it at any time, in any room. (Don't spend it all in one place!)
-
-          Type **INV** to see a list of items in your inventory.*`),
-          // using the dime randomly prints HEADS or TAILS
-          onUse() {
-            const side = Math.random() > 0.5 ? 'HEADS' : 'TAILS';
-            println(`You flip the dime. It lands on ${side}.`);
-          },
         }
       ],
       // places the player can go from this room
       exits: [
-        // GO NORTH command leads to the Reception Desk
-        { dir: 'north', id: 'reception' },
+        // GO GLADE command leads back to the Glade
+        { dir: 'glade', id: 'glade' },
       ],
     }
   ],
