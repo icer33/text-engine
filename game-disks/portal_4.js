3caa92a691c924ef18b13f4eaf1b875f21949c00
wgibson23 <128809757+wgibson23@users.noreply.github.com>
2023-04-07 10:23:50 -0400

    Update portal_4.js
    Corrected the room desc.

--- game-disks/portal_4.js
+++ game-disks/portal_4.js
@@ -7,8 +7,7 @@ const portal_4 = () => ({
             desc: `Portal 4's room. It is rather empty except for an orb floating in the middle of the room.`,
             onLook: () => {
                 const room = getRoom('portal_4');
-                room.desc = `Your eyes are drawn to a large, shimmering orb hovering in mid-air, its surface pulsing with a warm, golden light.
-         The room around you is dimly lit, but the orb seems to fill the space with a comforting radiance.`;
+                room.desc = `You enter a room with a shinning orb light, but as you approach you realize it is just a lamp missing its shade.`;
             },
             items: [
                 {
